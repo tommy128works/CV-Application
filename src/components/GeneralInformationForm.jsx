822eb2145dc8f3eb3d514d5810b1f5729db163d5
Tommy <tommy128works@gmail.com>
2023-08-09 12:58:18 -0400

    feat: add dropdown to form sections
    Added expand and collapse icons that will show the form section when
    clicked.

--- src/components/GeneralInformationForm.jsx
+++ src/components/GeneralInformationForm.jsx
@@ -1,23 +1,59 @@
-import "../styles/GeneralInformationForm.css";
+import "../styles/formSection.css";
+import expandMore from "../assets/expand_more.svg";
+import expandLess from "../assets/expand_less.svg";
 
 function GeneralInformationForm() {
-  // create form
-  // create dropdown feature
+
+  const toggleDropdown = () => {
+    let content = document.getElementById("myDropdown")
+    content.classList.toggle("show");
+
+    let icon = document.getElementById("toggle-icon");
+
+    if (content.classList.contains("show")) {
+      icon.src = expandLess;
+    } else {
+      icon.src = expandMore;
+    }
+
+  };
 
   return (
     <>
-      <form className="form-container">
+      <div>
+        <span className="section-title">General Information</span>
+        <button onClick={toggleDropdown} className="dropbtn">
+          <img id="toggle-icon" src={expandMore} alt="toggle drop down" />
+        </button>
+      </div>
+
+      <form id="myDropdown" className="dropdown-content">
         <label htmlFor="full_name">Full Name: </label>
-        <input type="text" id="full_name" name="full_name" placeholder="Enter your full name"></input>
+        <input
+          type="text"
+          id="full_name"
+          name="full_name"
+          placeholder="Enter your full name"
+        ></input>
 
         <label htmlFor="email">Email: </label>
         <input type="text" id="email" name="email" placeholder="Email"></input>
 
         <label htmlFor="phone_number">Phone Number: </label>
-        <input type="text" id="phone_number" name="phone_number" placeholder="Phone number"></input>
+        <input
+          type="text"
+          id="phone_number"
+          name="phone_number"
+          placeholder="Phone number"
+        ></input>
 
         <label htmlFor="city_province">City and province: </label>
-        <input type="text" id="city_province" name="city_province" placeholder="City, Province"></input>
+        <input
+          type="text"
+          id="city_province"
+          name="city_province"
+          placeholder="City, Province"
+        ></input>
       </form>
     </>
   );
